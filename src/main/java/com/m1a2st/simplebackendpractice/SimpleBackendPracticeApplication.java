ff88fd2123048c1e731174d08dd51987ee282623
m1a2st <s7133700@gmail.com>
2023-03-01 21:45:15 +0800

    init project

---
+++ src/main/java/com/m1a2st/simplebackendpractice/SimpleBackendPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.m1a2st.simplebackendpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBackendPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBackendPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m1a2st/simplebackendpractice/SimpleBackendPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1a2st.simplebackendpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBackendPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
