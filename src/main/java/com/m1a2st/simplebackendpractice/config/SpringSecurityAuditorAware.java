8bc340ecf2182d15286af1c45b9ef162ee8ade97
m1a2st <s7133700@gmail.com>
2023-03-19 23:12:30 +0800

    feat : create 可以使用@CreateBy @ModifiedDate @CreateDate 的SecurityAuditorAware類

---
+++ src/main/java/com/m1a2st/simplebackendpractice/config/SpringSecurityAuditorAware.java
@@ -0,0 +1,25 @@
+package com.m1a2st.simplebackendpractice.config;
+
+import com.m1a2st.simplebackendpractice.user.po.UserProfile;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+/**
+ * @Author m1a2st
+ * @Date 2023/3/19
+ * @Version v1.0
+ */
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.of("Test");
+        }
+        return Optional.of(authentication.getName());
+    }
+}
